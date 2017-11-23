8170f69c51f5978431247096cd27344d674d3b40
mark <fomin@infotek.ru>
2017-11-23 12:53:45 +0300

    common, eth: +шапка для tcp_server

--- trunk/samples/configsection/main.cpp
+++ trunk/samples/configsection/main.cpp
@@ -7,19 +7,21 @@ int main()
 {
     char val[MAXLENVALUE];
     int t = 0;
-
-    ConfigSection a("file:///home/roman/git_isp/infotek_git/trunk/samples/configsection/conf", "local");
+    //char const *file_conf = "file:///home/roman/git_isp/infotek_git/trunk/samples/configsection/conf";
+    char const *file_conf = "conf";
+    
+    ConfigSection a(file_conf, "local");
     t = a.u("timeout", 400);
     char const *tmp_buf1 = a.u("log", "failfailfailfailfailfailfailfailfailfailfailfailfailfailfail", val);
 
-    printf("%s\n", tmp_buf1);
-    printf("%d\n", t);
-    a.update("file:///home/roman/git_isp/infotek_git/trunk/samples/configsection/conf", "global");
+    printf("log: %s\n", tmp_buf1);
+    printf("timeout: %d\n", t);
+    a.update(file_conf, "global");
 
-    t = a.u("potr", 666);
+    t = a.u("port", 666);
     char const *tmp_buf2 = a.u("ip", "kroke", val);
 
-    printf("%s\n", tmp_buf2);
-    printf("%d\n", t);
+    printf("ip: %s\n", tmp_buf2);
+    printf("port: %d\n", t);
     return 0;
 }
